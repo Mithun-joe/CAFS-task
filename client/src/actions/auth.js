520f96cdd5755703aca224719a858f8c4d459d1f
Mithun <joieeemithun98@gmail.com>
2021-02-05 15:31:26 +0530

    error found and rectified

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {setAlert} from "./alert";
+import { setAlert } from "./alert";
 import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
@@ -14,67 +14,67 @@ import setAuthToken from "../utils/setAuthToken";
 
 //load user
 
-export const loadUser = () => async dispatch =>{
-    if(localStorage.token){
+export const loadUser = () => async dispatch => {
+    if (localStorage.token) {
         setAuthToken(localStorage.token)
     }
 
-    try{
-        const res = await axios.get('http://localhost:5000/api/auth');
+    try {
+        const res = await axios.get('api/auth');
 
         dispatch({
-            type:USER_LOADED,
+            type: USER_LOADED,
             payload: res.data
         })
-    }catch (err) {
+    } catch (err) {
         dispatch({
-            type:AUTH_ERROR
+            type: AUTH_ERROR
         })
     }
 };
 
-export const register = ({name,mobileNo,password}) => async dispatch =>{
+export const register = ({ name, mobileNo, password }) => async dispatch => {
 
-    const config = {
-        headers:{
-            'Content-Type' : 'application/json'
-        }
-    };
-
-    const body = JSON.stringify({name,mobileNo,password});
-
-    try{
-        const res = await axios.post('http://localhost:5000/api/users',body,config);
-
-        dispatch({
-            type:REGISTER_SUCCESS,
-            payload: res.data
-        });
-        dispatch(loadUser())
-    }catch (err) {
-        const errors = err.response.data.errors;
-
-        if(errors){
-            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
-        }
-
-        dispatch({
-            type:REGISTER_FAIL
-        })
-    }
-
-};
-
-export const login = (mobileNo,password) => async dispatch => {
     const config = {
         headers: {
             'Content-Type': 'application/json'
         }
     };
-    const body = JSON.stringify({ mobileNo, password});
+
+    const body = JSON.stringify({ name, mobileNo, password });
 
     try {
-        const res = await axios.post('http://localhost:5000/api/auth', body, config);
+        const res = await axios.post('api/users', body, config);
+
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
+        });
+        dispatch(loadUser())
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+        }
+
+        dispatch({
+            type: REGISTER_FAIL
+        })
+    }
+
+};
+
+export const login = (mobileNo, password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    const body = JSON.stringify({ mobileNo, password });
+
+    try {
+        const res = await axios.post('api/auth', body, config);
         dispatch({
             type: LOGIN_SUCCESS,
             payload: res.data
@@ -95,10 +95,10 @@ export const login = (mobileNo,password) => async dispatch => {
 
 export const logout = () => dispatch => {
     dispatch({
-        type:LOGOUT
+        type: LOGOUT
     });
 
     dispatch({
-        type:CLEAR_PROFILE
+        type: CLEAR_PROFILE
     })
 };
